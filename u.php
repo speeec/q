b0aa0cd7f6dc9a6df1f23e0cce03ce78c39f2605
speeec <58135809+speeec@users.noreply.github.com>
2019-11-24 14:09:38 +0300

    Delete u.php

--- u.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $fileLocation = getenv("DOCUMENT_ROOT") . "/myfile.txt";
-  $file = fopen($fileLocation,"w");
-  $content = "Your text here";
-  fwrite($file,$content);
-  fclose($file);
-?>
